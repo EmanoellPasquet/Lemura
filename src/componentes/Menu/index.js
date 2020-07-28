9233f6dc45a0c35bf34a92fca169d792fe472edd
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-07-28 19:45:43 -0300

    Setup inicial do projeto

---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Logo from "../../assets/img/Logo.png";
+import "./Menu.css";
+import Button from "../Button";
+//import ButtonLink from './componentes/ButtonLink';
+
+function Menu() {
+  return (
+    <header>
+      <nav className="Menu">
+        <a href="/">
+          <img className="Logo" src={Logo} alt="devflix logo" />
+        </a>
+
+        <Button as="a" className="ButtonLink" href="/">
+          Novo vídeo
+        </Button>
+      </nav>
+    </header>
+  );
+}
+
+export default Menu;
