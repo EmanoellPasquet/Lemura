164fe798cf2e2089e35bfad9f78a2162aeb3db20
Emanoell Pasquet <emanoell.pasquet@gmail.com>
2020-07-29 16:53:47 -0300

    Prosseguindo para o roteamento

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render( <
-    React.StrictMode >
-    <
-    App / >
-    <
-    /React.StrictMode>,
-    document.getElementById('root')
+import {  BrowserRouter, Switch, Route} from "react-router-dom";
+
+ReactDOM.render(
+    <BrowserRouter>
+    <Switch>
+        <Route path="/" component={App} exact/>
+        
+
+
+
+    </Switch>
+    
+    </BrowserRouter>,
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+  document.getElementById("root")
 );
